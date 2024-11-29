a1e3f2668692cfabf99613042ea2e5e9044d6f3f
glindaqu <q5481996@gmail.com>
2024-11-29 17:10:33 +0700

    basic project init

---
+++ main.py
@@ -0,0 +1,13 @@
+import pyvis as pv
+import networkx as nx
+
+
+graph = nx.Graph()
+points = [("a", "b", 1), ("b", "c", 1), ("a", "c", 1), ("c", "d", 1)]
+graph.add_weighted_edges_from(points)
+
+nx.draw(graph, with_labels = True)
+nt = pv.network.Network(height='99vh')
+nt.from_nx(graph)
+nt.show('graph.html')
+
