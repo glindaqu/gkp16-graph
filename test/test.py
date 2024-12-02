156cc2825a88b767498bfdecc93b6f6f036fa218
glindaqu <q5481996@gmail.com>
2024-12-02 18:02:20 +0700

    format

--- test/test.py
+++ test/test.py
@@ -4,9 +4,10 @@ from TestGraphBuilder import TestGraphBuilder
 
 def graph_builder_suite():
     suite = unittest.TestSuite()
-    suite.addTest(TestGraphBuilder('test_build'))
+    suite.addTest(TestGraphBuilder("test_build"))
     return suite
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     runner = unittest.TextTestRunner()
     runner.run(graph_builder_suite())
