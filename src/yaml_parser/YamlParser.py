e563ac154d93e576fa413568c04594ad45502bbf
glindaqu <q5481996@gmail.com>
2024-12-03 10:17:17 +0700

    parser file handling added

--- src/yaml_parser/YamlParser.py
+++ src/yaml_parser/YamlParser.py
@@ -9,8 +9,12 @@ class YamlParser:
             return YamlParserReturnCode.FILE_DOES_NOT_EXISTS
         if not os.access(path, os.R_OK):
             return YamlParserReturnCode.PERMISSION_DENIED
+        if os.path.splitext(path)[1] != ".yaml":
+            return YamlParserReturnCode.WRONG_EXTENSION
         with open(path, "r") as file:
             self.contents = yaml.safe_load(file)
+        if not self.contents["graph"]:
+            return YamlParserReturnCode.FILE_CONTAINS_UNEXPECTED_CONTENT
         return YamlParserReturnCode.OK
 
     def get_contents(self):
--- src/yaml_parser/YamlParserReturnCode.py
+++ src/yaml_parser/YamlParserReturnCode.py
@@ -5,3 +5,5 @@ class YamlParserReturnCode(enum.Enum):
     OK = 0
     PERMISSION_DENIED = -1
     FILE_DOES_NOT_EXISTS = -2
+    WRONG_EXTENSION = -3
+    FILE_CONTAINS_UNEXPECTED_CONTENT = -4
